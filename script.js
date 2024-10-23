1c6aaa306305d4fd2acb702d8ee44b878b1b9cec
Rotstein007 <felix04.hofer@gmail.com>
2024-10-23 18:00:58 +0200

    complete rearrangement

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function() {
-    const overlay = document.getElementById('loading-overlay');
-    const minLoadingTime = 500; // Minimum loading time in milliseconds
-
-    setTimeout(() => {
-        overlay.classList.add('hidden');
-    }, minLoadingTime);
-});
