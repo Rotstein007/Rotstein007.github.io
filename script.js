6fe8d73b1bef1daf2889228e624ec9a96f7dcdd0
Rotstein007 <felix04.hofer@gmail.com>
2024-11-11 18:26:13 +0100

    startet implementing arrow button

---
+++ script.js
@@ -0,0 +1,8 @@
+const arrow = document.querySelector('.arrow');
+
+arrow.addEventListener('click', function(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+});
