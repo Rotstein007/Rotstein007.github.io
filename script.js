b2541ca3ceaf39b04774a7477eede9e76b9f9c16
Rotstein <felix04.hofer@gmail.com>
2024-11-22 10:13:29 +0100

    page down arrow

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const arrow = document.querySelector('.arrow');
-
-arrow.addEventListener('click', function(event) {
-    event.preventDefault();
-    const targetId = this.getAttribute('href');
-    const targetElement = document.querySelector(targetId);
-    targetElement.scrollIntoView({ behavior: 'smooth' });
+document.addEventListener("DOMContentLoaded", () => {
+    const targetElement = document.querySelector('#content-section');
+    if (targetElement) {
+        console.log("Element gefunden:", targetElement);
+        document.querySelector('.arrow').addEventListener('click', (event) => {
+            event.preventDefault();
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        });
+    } else {
+        console.error("Element mit ID 'content-section' nicht gefunden.");
+    }
 });
