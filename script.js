ac6c87fc5d367ca91efe0117b41b8ecb7be9c7e3
Rotstein007 <felix04.hofer@gmail.com>
2024-11-23 17:36:58 +0100

    began to implement a circular selection of the topics

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
+/*document.addEventListener("DOMContentLoaded", () => {
     const targetElement = document.querySelector('#content-section');
     if (targetElement) {
         console.log("Element gefunden:", targetElement);
-        document.querySelector('.arrow').addEventListener('click', (event) => {
-            event.preventDefault();
-            targetElement.scrollIntoView({ behavior: 'smooth' });
-        });
+        const arrowElement = document.querySelector('.arrow');
+        if (arrowElement) {
+            arrowElement.addEventListener('click', (event) => {
+                event.preventDefault();
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            });
+        } else {
+            console.error("Element with class 'arrow' not found.");
+        }
     } else {
         console.error("Element mit ID 'content-section' nicht gefunden.");
     }
-});
+});*/
